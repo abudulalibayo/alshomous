35edc928f5de0af98f92f3514bbcfd028a742ae5
libtech <abudlalibayo@gmail.com>
2020-02-01 16:56:52 -0500

    Add files via upload

---
+++ pos_report_extended/__manifest__.py
@@ -0,0 +1,16 @@
+
+
+{
+    'name': 'POS Sale report extended',
+    'version': '11.0.0.0.0',
+    'license': 'AGPL-3',
+    'author': "Sigb",
+    'category': 'POS',
+    'depends': ['point_of_sale'],
+    'website': 'http://sociusigb.com/',
+    'data': [
+        'views/report.xml',
+
+    ],
+    'installable': True,
+}
